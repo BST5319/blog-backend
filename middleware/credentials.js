17434073360ff3c81e4ff2fc20451067e0dd2d1a
BST53 <burhan.s.tinwala@gmail.com>
2023-10-25 00:04:33 +0530

    Fixed Some CORS Policy

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,6 +4,9 @@ const credentials = (req, res, next) => {
     const origin = req.header.origin;
     if (allowedOrigins.includes(origin)) {
         res.headers("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x - client - key, x - client - token, x - client - secret, Authorization");
     }
     next();
 };
