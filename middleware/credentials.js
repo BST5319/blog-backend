b2221f981648e421ca446133859d7f36018e64ac
Burhanuddin S. Tinwala <113278150+BST5319@users.noreply.github.com>
2024-03-26 00:58:42 +0530

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,10 +1,10 @@
 const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
-    const origin = req.header.origin;
+    const origin = req.headers.origin;
     console.log(origin);
     if (allowedOrigins.includes(origin)) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Origin","*");
         res.setHeader("Access-Control-Allow-Credentials", true);
         // res.header("Access-Control-Allow-Origin", "*");
         // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
