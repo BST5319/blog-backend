fa67c8376d4400b06a3ba737242ccbf26f4e0f20
Burhanuddin S. Tinwala <113278150+BST5319@users.noreply.github.com>
2024-03-26 17:07:30 +0000

    Removed CORS origin validations

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,18 +1,13 @@
-const allowedOrigins = require("../config/allowedOrigins");
-
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin;
-    console.log("Request origin:", origin); // Log the origin
+    // Allow requests from any origin
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
 
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-        res.setHeader("Access-Control-Allow-Credentials", true);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-    }
     next();
 };
 
