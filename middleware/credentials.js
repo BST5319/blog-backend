db7db31c7914e94f23ab19182092e49d77d97a7d
Burhanuddin S. Tinwala <113278150+BST5319@users.noreply.github.com>
2024-03-26 16:51:00 +0000

    Fixed CORS for Vercel

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,10 +3,15 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     console.log("Request origin:", origin); // Log the origin
-    
+
     if (allowedOrigins.includes(origin)) {
         res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Credentials", true);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
     }
     next();
 };
