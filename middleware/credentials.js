5c2abfe638a8c27ef24d2a87e72e006a2883b7f3
Burhanuddin S. Tinwala <113278150+BST5319@users.noreply.github.com>
2024-03-26 16:31:04 +0000

    Fixed credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -2,13 +2,11 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
-    // console.log(origin);
+    console.log("Request origin:", origin); // Log the origin
+    
     if (allowedOrigins.includes(origin)) {
-        res.setHeader("Access-Control-Allow-Origin","*");
+        res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Credentials", true);
-        // res.header("Access-Control-Allow-Origin", "*");
-        // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x - client - key, x - client - token, x - client - secret, Authorization");
     }
     next();
 };
